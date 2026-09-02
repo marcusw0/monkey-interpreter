5e68c6ddf897a0feec54a52ebfc09a955ec2fca8
marcusw0 <54700785+marcusw0@users.noreply.github.com>
2026-09-01 23:25:56 -0400

    feat: add support for strings

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -89,6 +89,9 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
 
+	case *ast.StringLiteral:
+		return &object.String{Value: node.Value}
+
 	case *ast.Identifier:
 		return evalIdentifier(node, env)
 
